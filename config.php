9c7d8b4964b056e47cfb9b9e529f380f55e57243
krunal-08 <154742529+krunal-08@users.noreply.github.com>
2023-12-24 13:22:14 -0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$hostname = "http://selfiemedia.lovestoblog.com";
+
+$conn = mysqli_connect("sql306.epizy.com","epiz_33872837","Tc2L2V5UOsj","epiz_33872837_nimisha") or die("Connection failed : " . mysqli_connect_error());
+
+?>
