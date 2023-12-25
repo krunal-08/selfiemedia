24801815966447ea0e0c88e68443a2ba48cec439
krunal-08 <154742529+krunal-08@users.noreply.github.com>
2023-12-25 06:06:53 -0500

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$hostname = "http://selfiemedia.lovestoblog.com";
-
-$conn = mysqli_connect("sql306.epizy.com","epiz_33872837","Tc2L2V5UOsj","epiz_33872837_nimisha") or die("Connection failed : " . mysqli_connect_error());
-
-?>
